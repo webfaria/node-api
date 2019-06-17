f01f2fb09410c84da4eee1dcbff1e79e7e047089
webfaria <web_faria@yahoo.com.br>
2019-06-17 16:17:06 -0300

    Conectando database com Mongoose

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require('mongoose');
 
+//iniciando o App
 const app = express();
 
+//inicando o BD
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+
+//primeira rota
 app.get('/',(req, res) => {
 res.send("Hello Rocksetseat");
 

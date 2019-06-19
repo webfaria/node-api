0ceef6d7d10ec3de1d4380fffd4f11f7910dc12a
webfaria <web_faria@yahoo.com.br>
2019-06-19 12:31:33 -0300

    Crud completo

--- src/controllers/ProdutoController.js
+++ src/controllers/ProdutoController.js
@@ -2,15 +2,28 @@ const mongoose = require('mongoose');
 const Produto = mongoose.model('Produto');
 
 module.exports = {
-    async index(req, res) {
+    async index(req, res){
         const produtos = await Produto.find();
-
         return res.json(produtos);
+    },
+
+    async show(req, res){
+        const produto = await Produto.findById(req.params.id);
+        return res.json(produto);
     },
 
     async store(req, res){
         const produto = await Produto.create(req.body);
-
         return res.json(produto);
+    },
+
+    async update(req, res){
+        const produto = await Produto.findByIdAndUpdate(req.params.id, req.body, { new:true });
+        return res.json(produto);
+    },
+
+    async delete(req, res){
+        const produto = await Produto.findByIdAndRemove(req.params.id);
+        return res.send();
     }
 };
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,9 @@ const routes = express.Router();
 const ProdutoController = require('./controllers/ProdutoController')
 
 routes.get("/produtos", ProdutoController.index);
+routes.get("/produtos/:id", ProdutoController.show);
 routes.post('/produtos', ProdutoController.store);
+routes.put('/produtos/:id', ProdutoController.update);
+routes.delete('/produtos/:id', ProdutoController.delete);
 
 module.exports = routes;
